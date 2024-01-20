a582395e5b591664104d697e0a4f558e839a97b2
Vinicius Coelho <vinicius2001@gmail.com>
2024-01-20 14:18:01 -0300

    feat - create and index action

--- app/controllers/api/v1/dns_records_controller.rb
+++ app/controllers/api/v1/dns_records_controller.rb
@@ -1,14 +1,25 @@
 module Api
   module V1
     class DnsRecordsController < ApplicationController
-      # GET /dns_records
       def index
-        # TODO: Implement this action
+        dns_records = DnsRecord.all
+
+        render json: dns_records, status: :ok
       end
 
-      # POST /dns_records
       def create
-        # TODO: Implement this action
+        dns_record = DnsRecord.new(dns_record_params)
+        if dns_record.save
+          render json: { id: dns_record.id }, status: :created
+        else
+          render json: dns_record.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def dns_record_params
+        params.require(:dns_records).permit(:ip, hostnames_attributes: [:hostname])
       end
     end
   end
